18b91dfb5a8cc1ce738b654e62208ac5f97c925b
mar1kxs <maksim234.kyky@gmail.com>
2025-06-10 16:11:11 +0200

    btn

--- public/src/scripts/main.js
+++ public/src/scripts/main.js
@@ -1,8 +1,6 @@
 const btn = document.getElementById("toggle-btn");
-const dropdown = document.getElementById("dropdown");
 
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
   btn.classList.toggle("closed");
-  dropdown.classList.toggle("visible");
 });
